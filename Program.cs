a2e3d7878683cb4c73b517bd77cad49edb9b12e8
Iaroslav Shubert <rocketdeath@ukr.net>
2016-08-25 17:50:17 +0300

    add guid generation,  file dir

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace BioFileServer
 {
@@ -7,18 +8,22 @@ namespace BioFileServer
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var logPath = System.IO.Path.GetTempFileName();
-            Console.WriteLine(logPath);
+
+            var currentDir = Directory.GetCurrentDirectory();
+            var dirPath = Path.Combine(currentDir, "files");
+            Directory.CreateDirectory(dirPath);
+            var fileName = Guid.NewGuid().ToString();
+            var filePath = System.IO.Path.Combine(dirPath, fileName);
+            
+            Console.WriteLine(filePath);
 
             string text = Console.ReadLine();
             while (text != "#q")
             {
-                Utils.WriteFile(logPath, text);
-                Console.WriteLine(Utils.ReadFile(logPath));
+                Utils.WriteFile(filePath, text);
+                Console.WriteLine(Utils.ReadFile(filePath));
                 text = Console.ReadLine();
             }
         }
-
-        
     }
 }
