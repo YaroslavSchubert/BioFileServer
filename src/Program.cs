36772e4dc50b673b2678cfa57cb6bd578796ba3c
Iaroslav Shubert <rocketdeath@ukr.net>
2016-08-25 19:24:50 +0300

    grpc server

---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Services;
+
+namespace Bioskynet
+{
+    public class Program
+    {
+        static FileServiceManager fileServiceManager = new FileServiceManager();
+        public static void Main(string[] args)
+        {
+            fileServiceManager.Start();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            fileServiceManager.Stop();
+            // var currentDir = Directory.GetCurrentDirectory();
+            // var dirPath = Path.Combine(currentDir, "files");
+            // Directory.CreateDirectory(dirPath);
+            // var fileName = Guid.NewGuid().ToString();
+            // var filePath = System.IO.Path.Combine(dirPath, fileName);
+
+            // Console.WriteLine(filePath);
+
+            // string text = Console.ReadLine();
+            // while (text != "#q")
+            // {
+            //     Utils.WriteFile(filePath, text);
+            //     Console.WriteLine(Utils.ReadFile(filePath));
+            //     text = Console.ReadLine();
+            // }
+        }
+    }
+}
--- src/Utils.cs
+++ src/Utils.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 
-namespace BioFileServer
+namespace Bioskynet
 {
     class Utils
     {
