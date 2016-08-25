b16574126412e5c31e373026e8fc03df42fdb9c1
Iaroslav Shubert <rocketdeath@ukr.net>
2016-08-25 14:30:41 +0300

    add file read/write

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace ConsoleApplication
+namespace BioFileServer
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var logPath = System.IO.Path.GetTempFileName();
+            Console.WriteLine(logPath);
+
+            string text = Console.ReadLine();
+            while (text != "#q")
+            {
+                Utils.WriteFile(logPath, text);
+                Console.WriteLine(Utils.ReadFile(logPath));
+                text = Console.ReadLine();
+            }
         }
+
+        
     }
 }
