f2f1497a37b55942adce96cdf4b97d9e51596c7a
Yaroslav Schubert <YaroslavShubert@gmail.com>
2016-11-29 14:14:46 +0200

    add publish for ubuntu, win10-x86, win10-x64

--- src/FileServer/build.cmd
+++
@@ -1,3 +0,0 @@
-dotnet restore
-dotnet build -c debug
-dotnet build -c release
---
+++ src/FileServer/publish.cmd
@@ -0,0 +1,4 @@
+dotnet restore
+dotnet publish -c release -r win10-x86
+dotnet publish -c release -r win10-x64
+dotnet publish -c release -r ubuntu.14.04-x64
